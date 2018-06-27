115b36bf20145e58a3b97ff46151fe28368042c6
Vashisht Madhavan <vashisht@uber.com>
2018-06-27 11:12:03 -0700

    adding bandit test script

---
+++ maml_examples/maml_bandit.py
@@ -0,0 +1,53 @@
+from sandbox.rocky.tf.algos.maml_trpo import MAMLTRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.envs.gym_env import GymEnv
+from rllab.envs.normalized_env import normalize
+from rllab.misc.instrument import stub, run_experiment_lite
+from sandbox.rocky.tf.policies.maml_minimal_categorical_mlp_policy import MAMLCategoricalMLPPolicy
+from sandbox.rocky.tf.envs.base import TfEnv
+import tensorflow as tf
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', type=str, default='Bandit_k5_n10-v0')
+parser.add_argument('--expt_name', type=str, default='debug')
+parser.add_argument('--seed', type=int, default=0)
+args = parser.parse_args()
+
+stub(globals())
+env = TfEnv(normalize(GymEnv(args.env, record_video=False, record_log=False)))
+
+policy = MAMLCategoricalMLPPolicy(
+    name="policy",
+    env_spec=env.spec,
+    grad_step_size=0.1,
+    hidden_nonlinearity=tf.nn.relu,
+    hidden_sizes=(100, 100),
+)
+baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+algo = MAMLTRPO(
+    env=env,
+    policy=policy,
+    baseline=baseline,
+    batch_size=20, # number of trajs for grad update
+    max_path_length=200,
+    meta_batch_size=40,
+    num_grad_updates=1,
+    n_itr=800,
+    use_maml=True,
+    step_size=0.01
+)
+
+run_experiment_lite(
+    algo.train(),
+    exp_prefix=args.expt_name,
+    exp_name='run_{}'.format(args.seed),
+    n_parallel=8,
+    snapshot_mode="gap",
+    snapshot_gap=100,
+    python_command='python3',
+    seed=args.seed,
+    mode="local",
+    # plot=True,
+)
